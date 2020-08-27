693f4d00209a39d73b8f71ba8a7efad1ed5e01b8
William Pannell <lpann5@gmail.com>
2020-08-27 08:16:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  enable :sessions
+  set :session_secret, "secret"
+
+  get '/hey' do
+    @session = session
+    erb :index
+  end
 end
